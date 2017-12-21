2bbd160003f1d85363523bad58472c1c33256415
wangwei <892458829@qq.com>
2017-12-21 16:08:57 +0800

    add

--- mustache.js
+++ mustache.js
@@ -1,22 +1,25 @@
- function mustache(data,elment) {
-    	var el = elment? elment:this.templete ;
-        var elment = document.querySelector(el)
-        
-        var string = elment.innerHTML;
-        var reg = /\{\{(\w+)\}\}/g
-        var newstring = string.replace(reg, function() {
-            console.log(arguments)
-            var key = arguments[1]
-            return data[key]
+ function mustache(object) {
+     var el = object.el ? object.el : this.templete;
+     var elment = document.querySelector(el)
 
-        })
-        elment.innerHTML = newstring;
-       console.log(newstring)
+     var string = elment.innerHTML;
+     var reg = /\{\{(\w+)\}\}/g
+     var newstring = string.replace(reg, function() {
+         console.log(arguments)
+         var key = arguments[1]
+         return data[key]
 
-    }
-export {mustache}
-    // stringFormate({
-    //     name: 'wang',
-    //     age: 12,
-    //     q:'qweqrterwrq'
-    // },'.box')
+     })
+     elment.innerHTML = newstring;
+     console.log(newstring)
+
+ }
+ export { mustache }
+ // stringFormate({
+ // 	el:'.box'
+ //     data: {
+ //         name: 'wang',
+ //         age: 12,
+ //         q: 'qweqrterwrq'
+ //     }
+ // })
