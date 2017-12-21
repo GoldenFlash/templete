b1949b1191e9b430ab115452de7557864b992560
wangwei <892458829@qq.com>
2017-12-21 15:24:52 +0800

    add

---
+++ stingFormate.js
@@ -0,0 +1,16 @@
+function stringFormate(string) {
+ 	var params = [].slice.call(arguments, 1);
+ 	console.log(params)
+ 	var reg = /\{(\d+)\}/g
+ 	var newstring = string.replace(reg, function() {
+ 		// console.log(arguments)
+ 		var index = arguments[1] - 1
+ 		console.log(index)
+ 		return params[index]
+
+ 	})
+ 	console.log(newstring)
+
+ }
+ export {stringFormate}
+ // stringFormate('{3} {1} {2}', 'hi', 'pp', 'aa')
